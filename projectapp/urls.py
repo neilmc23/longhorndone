fd95aa5d3ee3b163c8cb0a8960b88d7aa0bc1af5
Neil McHenry <neilmchenry@Neils-MacBook-Pro-2.local>
2020-04-30 14:56:24 -0500

    message1

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -5,5 +5,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home_page, name='home_page'),
+    path('course_list_page/', views.course_list_page, name='course_list_page'),
+    path('professor_list_page/', views.professor_list_page, name='professor_list_page'),
+    path('student_list_page/', views.student_list_page, name='student_list_page'),
+    path('course_page/', views.course_page, name='course_page'),
+    path('professor_page/', views.professor_page, name='professor_page'),
+    path('student_page/', views.student_page, name='student_page'),
+    path('project_page/', views.project_page, name='project_page')
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -3,6 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import *
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def home_page(request):
+    return HttpResponse("Hello, world. You're at the home page.")
+    pass
+
+def course_list_page(request):
+    return HttpResponse("Hello, world. You're at the course list page.")
+    pass
+
+def professor_list_page(request):
+    return HttpResponse("Hello, world. You're at the professor list page.")
+    pass
+
+def student_list_page(request):
+    pass
+
+def course_page(request):
+    pass
+
+def professor_page(request):
+    pass
+
+def student_page(request):
+    pass
+
+def project_page(request):
+    pass
